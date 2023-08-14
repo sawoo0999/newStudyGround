7482590911e85b8f5c7544d07c23387512c31bf1
sawoo0999 <sawoo0999@naver.com>
2023-08-14 21:59:09 +0900

    Java Script 8

--- JavaScript/JS/JS.js
+++ JavaScript/JS/JS.js
@@ -27,6 +27,28 @@ function tapopen(구멍) {
         });
 }
 
+var car = { name: '소나타', price: [50000, 3000, 4000], color: 'white' };
+
+// document.querySelector('.name').innerHTML = car.name;
+// document.querySelector('.price').innerHTML = car.price[0];
+
+var products = [
+    { id: 0, price: 70000, title: 'Blossom Dress' },
+    { id: 1, price: 50000, title: 'Springfield Shirt' },
+    { id: 2, price: 60000, title: 'Black Monastery' },
+];
+
+for (let i = 0; i < 3; i++) {
+    card(i);
+}
+
+function card(구멍) {
+    var title = document.querySelectorAll('.card-body h5')[구멍];
+    var price = document.querySelectorAll('.card-body p')[구멍];
+    title.innerHTML = products[구멍].title;
+    price.innerHTML = '가격 : ' + products[구멍].price;
+}
+
 // $('.tab-button')
 //     .eq(0)
 //     .on('click', function () {
