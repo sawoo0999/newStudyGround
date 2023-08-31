aaef505f85d39917741b3dd217a0e2684eb35f26
sawoo0999 <sawoo0999@naver.com>
2023-08-31 20:44:00 +0900

    React 3

--- React/blog/src/App.js
+++ React/blog/src/App.js
@@ -4,14 +4,16 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  let post = "강남 우동 맛집";
   let [글제목, b] = useState([
     "남자 코트 추천",
     "강남 우동 맛집",
     "파이썬독학",
   ]);
   let [like, plu] = useState([0, 0, 0]);
-  let [modal, setModal] = useState([false, false, false]);
+  let [modal, setModal] = useState(false);
+  let [tilte, settitle] = useState(0);
+  let [content, setcontent] = useState("");
+
   function view() {
     setModal(!modal);
   }
@@ -30,6 +32,21 @@ function App() {
     let copy = [...글제목];
     copy[0] = "여자 코트 추천";
     b(copy);
+  }
+
+  function arr() {
+    let copy = [...글제목];
+    if (content != "") {
+      copy.unshift(content);
+    }
+    b(copy);
+    console.log(글제목);
+  }
+
+  function likecopy() {
+    let copy = [...like];
+    copy.unshift(0);
+    plu(copy);
   }
 
   return (
@@ -59,36 +76,72 @@ function App() {
       {글제목.map((a, i) => {
         return (
           <div className="list" key={i}>
-            <h4 onClick={view}>
-              {a}
+            <h4
+              onClick={() => {
+                view();
+                settitle(i);
+              }}
+            >
+              {글제목[i]}
               <span
-                onClick={() => {
+                onClick={(e) => {
                   let copylike = [...like];
                   copylike[i] += 1;
                   plu(copylike);
                   console.log(copylike);
+                  e.stopPropagation();
                 }}
               >
                 ❤
               </span>
               {like[i]}
             </h4>
-            <p>8월 28일 발행</p>
+            <p>현재 날짜</p>
+            <button
+              onClick={() => {
+                let copy = [...글제목];
+                let copyplu = [...like];
+                copyplu.splice(i, 1);
+                copy.splice(i, 1);
+                b(copy);
+                plu(copyplu);
+                console.log(글제목);
+              }}
+            >
+              삭제
+            </button>
           </div>
         );
       })}
 
-      {modal == true ? <Modal /> : null}
+      <input
+        onChange={(e) => {
+          setcontent(e.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          arr();
+          likecopy();
+        }}
+      >
+        발행
+      </button>
+
+      {modal == true ? (
+        <Modal nam={글제목} tilte={tilte} woman={woman} />
+      ) : null}
     </div>
   );
 }
 
-function Modal() {
+function Modal(props) {
   return (
     <div className="modal">
-      <h4>제목</h4>
+      <h4>{props.nam[props.tilte]}</h4>
       <p>날짜</p>
       <p>상세내용</p>
+      <button onClick={props.woman}>글수정</button>
     </div>
   );
 }
