0adb5302cd0030230c4bd1afc6accc77f0607001
sawoo0999 <sawoo0999@naver.com>
2023-10-12 16:23:46 +0900

    NEXT 3

--- NEXT/fresh/app/cart/page.js
+++ NEXT/fresh/app/cart/page.js
@@ -1,7 +1,22 @@
 export default function Cart() {
+  let item = ["Tomatoes", "Pasta", "Coconut"];
+
   return (
     <div>
-      <h4 className="title">카트 페이지</h4>
+      <h4 className="title">Cart</h4>
+      {item.map((a, i) => {
+        return <Cartitem item={item[i]} />;
+      })}
+    </div>
+  );
+}
+
+function Cartitem(props) {
+  return (
+    <div className="cart-item">
+      <p>{props.item}</p>
+      <p>$40</p>
+      <p>1개</p>
     </div>
   );
 }
--- NEXT/fresh/app/cart/payment/page.js
+++
@@ -1,7 +0,0 @@
-export default function Payment() {
-  return (
-    <div>
-      <h4 className="title">결제 페이지 입니다.</h4>
-    </div>
-  );
-}
