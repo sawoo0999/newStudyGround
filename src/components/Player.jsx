36aa0351d599cdfc20fa6c6d4247f4b6b7f9affa
Lee jihun <sawoo0999@naver.com>
2024-06-17 23:11:31 +0900

    first commit

---
+++ src/components/Player.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+export default function Player({ initialName, symbol, isActive }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+  function handleEditClick() {
+    setIsEditing((editing) => !editing);
+    //setIsEditing(!isEditing)   이렇게 사용하면 아무리 여러개를 써도 상태값은 처음 쓴 결과랑 똑같이 나온다
+    //setIsEditing(!isEditing) false -> true
+    //setIsEditing(!isEditing) false -> true
+    //setIsEditing((editing) => !editing); false -> true
+    //setIsEditing((editing) => !editing); true -> false
+  }
+
+  //input value가 변경될때 마다 setPlayerName에 value 값 반환
+  function handleChange(e) {
+    setPlayerName(e.target.value);
+  }
+
+  let editablePlayerName = isEditing ? (
+    <input type="text" required value={playerName} onChange={handleChange} />
+  ) : (
+    <span className="player-name">{playerName}</span>
+  );
+  let btnCaption = !isEditing ? "Edit" : "Save";
+  return (
+    <li className={isActive ? "active" : ""}>
+      <span className="player">
+        {editablePlayerName}
+        <span className="player-symbol">{symbol}</span>
+        <button onClick={handleEditClick}>{btnCaption}</button>
+      </span>
+    </li>
+  );
+}
