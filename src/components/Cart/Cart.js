54302e577fae9e540e2e5583fc924df6d9428e70
Lee jihun <sawoo0999@naver.com>
2024-08-12 22:57:18 +0900

    first commit

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,31 @@
+import { useSelector } from "react-redux";
+import Card from "../UI/Card";
+import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
+
+const Cart = (props) => {
+  const cartItems = useSelector((state) => state.cart.items);
+  console.log(cartItems);
+  return (
+    <Card className={classes.cart}>
+      <h2>Your Shopping Cart</h2>
+      <ul>
+        {cartItems.length === 0 && <p>No Quantity </p>}
+        {cartItems.map((item) => (
+          <CartItem
+            key={item.itemId}
+            item={{
+              id: item.itemId,
+              title: item.name,
+              quantity: item.quantity,
+              total: item.totalPrice,
+              price: item.price,
+            }}
+          />
+        ))}
+      </ul>
+    </Card>
+  );
+};
+
+export default Cart;
