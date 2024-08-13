c9e35f5d500998609cfd81eb3ba98245b3582837
Lee jihun <sawoo0999@naver.com>
2024-08-13 22:45:20 +0900

    basic Router

---
+++ src/pages/Products.js
@@ -0,0 +1,14 @@
+function ProductsPage() {
+  return (
+    <>
+      <h1>The Products Page</h1>
+      <ui>
+        <li>Products 1</li>
+        <li>Products 2</li>
+        <li>Products 3</li>
+      </ui>
+    </>
+  );
+}
+
+export default ProductsPage;
